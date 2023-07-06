9b5c9fe9156f24f8b18253101f0bd4973d41b620
EMRE GÜLBAY <20190808071@ogr.akdeniz.edu.tr>
2023-07-06 19:03:59 +0300

    sort buttons added

--- Js/Js.js
+++ Js/Js.js
@@ -2,31 +2,30 @@ var data = localStorage.getItem('data')
 
 data = JSON.parse(data)
 //data.response.itineraryList[0].flightOptionList.forEach(element => {});
-prices = [];
-data.response.itineraryList[0].flightOptionList.forEach( x => {
-  prices.push(x.fareOptionList[0].originalPrice)
 
-})
-prices.sort( (a,b) => { return a-b } );
-flights = [];
-data.response.itineraryList[0].flightOptionList.forEach(x => flights.push(x))
-
-document.addEventListener("DOMContentLoaded", function() {
-  const body = document.body;
+var flights = data.response.itineraryList[0].flightOptionList;
+populateTable();
+function populateTable() {
   const table = document.getElementById('flightTable');
+  const rows = table.querySelectorAll('tr');
+  rows.forEach(row => {
+    if (!row.querySelector('th')) {
+      row.remove();
+    }
+  });
   flights.forEach(flight => {
     const row = document.createElement('tr');
+
+    const flightNo = flight.flightNo;
+    const departureAirportCode = flight.departureAirport.code;
+    const arrivalAirportCode = flight.arrivalAirport.code;
+    const flightOperatorName = flight.flightList[0].flightOperator.name;
+    const originalPrice = flight.fareOptionList[0].originalPrice;
+    const way = flight.flightList.length > 1 ? "direct" : "connecting";
     const button = document.createElement("button");
     button.className = "asd";
     button.innerHTML= 'Sec';
-    const flightNo = flight.flightNo;
-    const departureAirport = flight.departureAirport.code;
-    const arrivalAirport = flight.arrivalAirport.code;
-    const flightOperatorName = flight.flightList[0].flightOperator.name;
-    const originalPrice = flight.fareOptionList[0].originalPrice;
-    const way = flight.flightList.length > 1 ? "direkt" : "aktarmali"; 
-    const cells = [flightNo, departureAirport, arrivalAirport, way, flightOperatorName, originalPrice];
-    
+    const cells = [flightNo, departureAirportCode, arrivalAirportCode, way, flightOperatorName, originalPrice];
     cells.forEach(cellText => {
       const cell = document.createElement('td');
       cell.textContent = cellText;
@@ -35,11 +34,35 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     table.appendChild(row);
-});
-});
+  });
+}
+function sortFlightsByPriceLow() {
+  flights.sort((a, b) => {
+    const priceA = a.fareOptionList[0].originalPrice;
+    const priceB = b.fareOptionList[0].originalPrice;
+    return priceA - priceB;
+  });
 
+  populateTable();
+}
+const sortLowButton = document.getElementById('sortButtonLow');
+sortLowButton.addEventListener('click', sortFlightsByPriceLow);
 
+function sortFlightsByPriceHigh() {
+  flights.sort((a, b) => {
+    const priceA = a.fareOptionList[0].originalPrice;
+    const priceB = b.fareOptionList[0].originalPrice;
+    return priceB - priceA;
+  });
 
+  populateTable();
+}
+const sortLargeButton = document.getElementById('sortButtonLarge');
+sortLargeButton.addEventListener('click', sortFlightsByPriceHigh);
+
+populateTable();
+
+  
 
 
 
